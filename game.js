4d35e2f79d4581c10f8e2592ed3a1eee83b8061e
Monica Choe <monicchoe@gmail.com>
2017-06-09 12:22:11 -0400

    colors

--- game.js
+++ game.js
@@ -2,7 +2,6 @@ var mainElement = document.getElementById('main')
 if (mainElement) {
   var game = Life(mainElement)
 
-  // Connect #step_btn to the step function
   document.getElementById('step_btn')
   .addEventListener('click', game.step);
   document.getElementById('play_btn')
@@ -11,29 +10,20 @@ if (mainElement) {
   .addEventListener('click', game.random);
   document.getElementById('clear_btn')
   .addEventListener('click', game.clear);
-  // TODO: Connect other buttons.
 }
 
 function Life(container, width=12, height=12) {
-  // Create boards for the present and future.
-  // Game boards are somewhat expensive to create, so we're going
-  // to be reusing them. Each time we step the game, `future`
-  // becomes `present` and vice versa.
   var present = new Board(width, height);
   var future = new Board(width, height);
 
-  // Create a <table> to hold our cells.
   var table = createTable();
   var timer, playing = false; 
   
-  // Put the table in our container
   container.appendChild(table);
 
-  // Add a mouse down listener to our table
   table.addEventListener('mousedown', toggleCellFromEvent)
 
   function createTable() {
-    // create <table> element
     var table = document.createElement('table');       // <table
     table.classList.add('board')                       //   class='board'>
     for (var r = 0; r < height; r++) {
@@ -53,24 +43,12 @@ function Life(container, width=12, height=12) {
   }
   
   function toggleCellFromEvent(event) {
-    // FIXME: This currently always toggles cell (0, 0).
-    // How do we get the coordinate of the cell that was clicked on?
-    // HINT: https://developer.mozilla.org/en-US/docs/Web/API/Event/target
-    var cell = document.getElementById(event.target.id); // ⬅️ Fix me
+    var cell = document.getElementById(event.target.id); 
     present.toggle(cell.coord);
     paint();
   }
 
   function paint() {
-    // TODO:
-    //   1. For each <td> in the table:
-    //     a. If its cell is alive, give the <td> the `alive` CSS class.
-    //     b. Otherwise, remove the `alive` class.
-    //
-    // To find all the <td>s in the table, you might query the DOM for them, or you
-    // could choose to collect them when we create them in createTable.
-    //
-    // HINT:
     //   https://developer.mozilla.org/en-US/docs/Web/API/Element/classList
     //   https://developer.mozilla.org/en-US/docs/Web/API/Element/getElementsByTagName
     var cells = table.getElementsByTagName("td"); 
@@ -80,80 +58,45 @@ function Life(container, width=12, height=12) {
       }
       else{
         cells[i].classList.remove("alive");
+        cells[i].classList.add("dead");
       }
     }
   }
 
   function step() {
-    // Hello, destructuring assignment:
     //   https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Destructuring_assignment\
-    [present, future] = tick(present, future);  // tick is from board.js
-    // ⬆️ Why is there a semicolon at the beginning of this line?
-    //
-    // It's not necessary, but we have it there to avoid a confusing problem.
-    // Let's say you put a console.log at the start of this function, but
-    // forget the semicolon at the end of the line:
-    //
-    //     console.log('entered step')
-    //     [present, future] = tick(present, future);
-    //
-    // This is the one case in which automatic semicolon insertion can really bite
-    // you. Since the line begins with a `[` token, JS will assume that it *continues*
-    // the previous expression. That is, it will condense your code to this:
-    //
-    //     console.log('entered step')[present, future] = tick(present, future);
-    //
-    // Which will throw the confusing error:
-    //
-    //     TypeError: Cannot read property '#<Board>' of undefined
-    //
-    // Since it's trying to look up `present` inside the return value of `console.log`,
-    // which is undefined.
-    //
-    // In general, you should either be fastidious about ending lines with semicolons,
-    // or you should be sure to *begin* with a semicolon any line that could continue
-    // an expression—that's any line that starts with a (, [, or ` character.
-
-    // Paint the new present
+    [present, future] = tick(present, future); 
     paint();
   }
 
   function play() {
-    // TODO:
-    // Start playing by running the `step` function    
-    // automatically repeatedly every fixed time interval
+    // https://developer.mozilla.org/en-US/docs/Web/API/WindowOrWorkerGlobalScope/setInterval
     playing = true;
     timer = setInterval(step, 500);
-    // HINT:
-    // https://developer.mozilla.org/en-US/docs/Web/API/WindowOrWorkerGlobalScope/setInterval
   }
 
   function stop() {
-    // TODO: Stop autoplay.
-    // HINT:
     // https://developer.mozilla.org/en-US/docs/Web/API/WindowOrWorkerGlobalScope/clearInterval
     playing = false;
     clearInterval(timer);
   }
 
   function togglePlaying() {
-    // TODO: If we're playing, stop. Otherwise, start playing.
     if (playing){
       stop();
-   } else{
-    play();
-   }
+    } 
+    else{
+      play();
+    }
   }
 
   function clear() {
-    // TODO: Clear the board
     function everythingDies() { return false }
     [present, future] = tick(present, future, everythingDies);
     paint();
   }
 
   function random() {
-    // TODO: Randomize the board
     function randomize() {return (Math.floor(Math.random()*2)===1) ? true : false}
     [present, future] = tick(present, future, randomize);
     paint();
